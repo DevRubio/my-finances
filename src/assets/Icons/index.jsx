f608072b95881ebb6642c252fd53a546a78a7b11
oscar.franco <ofrubio0@gmail.com>
2023-10-14 14:01:26 -0500

    Se crea componente Navbar y las rutas correspondientes a la App, se crea los assets, se retira los estilos predefinidos de la App

---
+++ src/assets/Icons/index.jsx
@@ -0,0 +1,18 @@
+export const IconPlus = () =>(
+    <svg className="w-1 h-1 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M9.546.5a9.5 9.5 0 1 0 9.5 9.5 9.51 9.51 0 0 0-9.5-9.5ZM13.788 11h-3.242v3.242a1 1 0 1 1-2 0V11H5.304a1 1 0 0 1 0-2h3.242V5.758a1 1 0 0 1 2 0V9h3.242a1 1 0 1 1 0 2Z"/>
+  </svg>
+)
+
+export const IconWallet = () =>(
+  <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z"/>
+    <path d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z"/>
+  </svg>
+)
+
+export const IconUser = () =>(
+  <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+  </svg>
+)
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+'use client'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { IconUser } from "@/assets/Icons"
+const links =[
+    {
+        label: 'Panel de Control',
+        route: '/'
+    },
+    {
+        label: 'Cuentas',
+        route: '/accounts'
+    },
+    {
+        label: 'Registros',
+        route: '/records'
+    },
+    {
+        label: 'Estadísticas',
+        route: '/analytics'
+    }
+]
+
+export function Navbar(){
+    const active = 'text-gray-800 font-semibold'
+    const noActive = 'text-gray-500'
+    const pathname = usePathname()
+    return(        
+        <header className="border rounden-md p-4">
+            <nav className="flex justify-between">
+                <ul className="flex list-none gap-8">
+                    {links.map(({label, route})=>(
+                        <li key={route} >
+                            <Link href={route} className={`${pathname === route ? active : noActive}`}>
+                                {label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                <IconUser/>
+            </nav>
+
+        </header>
+    )
+}
