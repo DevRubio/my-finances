58919b3a83efcdd9e0a2a5629fd9089e8e736de4
oscar.franco <ofrubio0@gmail.com>
2023-10-20 20:48:43 -0500

    (UPDATE) Se realiza el llamado a la base de Datos y se muestra la informacion de las Accounts

--- src/components/Accounts/index.jsx
+++ src/components/Accounts/index.jsx
@@ -1,34 +1,29 @@
 import { Grid } from "@tremor/react"
 import { CardAccounts } from "./CardAccounts"
 import { AddAccount } from "./AddAccount"
+import { firebaseApp } from "@/app/firebase"
+import { collection, getDocs, getFirestore } from "firebase/firestore"
+
+const db = getFirestore(firebaseApp)
+const querySnapshot = await  getDocs(collection(db,'accounts'));
+const acounts = []
+querySnapshot.forEach((doc) => {
+    acounts.push({...doc.data(), id: doc.id})
+})
 
 export function Accounts(){
-    const acounts = [
-        {
-            name: "Efectivo",
-            balance: "500000",
-            color: "bg-[#1fb6ff]",
-            icon: "CurrencyDollarIcon"
-        },
-        {
-            name: "Cdt 1",
-            balance: "65000.01",
-            color: "bg-[#d32f2fff]",
-            icon: "CreditCardIcon"
-        },
-        
-    ]
+    
+
     return(
         <div className="bg-slate-100 p-2">   
             <Grid numItems={1} numItemsSm={2} numItemsLg={5} className="gap-2 m-4">
                 {acounts.map((item) =>(
                     <CardAccounts
-                        key={item.name}
+                        key={item.id}
                         color={item.color}
                         icon={item.icon}
                         name={item.name}
-                        balance={item.balance}
-
+                        balance={item.amount}
                     />
                 ))}
                 <AddAccount/>
