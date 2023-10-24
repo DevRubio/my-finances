169ff4825d41d9272a01f1e03dd525e6f51fdaae
oscar.franco <ofrubio0@gmail.com>
2023-10-24 07:24:44 -0500

    (UPDATE) Se cambia la obtencion de los Icons utilizando el componente GetIcons

--- src/components/Accounts/CardAccounts.jsx
+++ src/components/Accounts/CardAccounts.jsx
@@ -1,26 +1,33 @@
-import { CashIcon, CreditCardIcon, CurrencyDollarIcon, OfficeBuildingIcon } from "@heroicons/react/outline"
+'use client'
+import { PencilIcon } from "@heroicons/react/outline"
+import { GetIcons } from "../GetIcons";
 export function CardAccounts(props){
-    let IconCard = ""
-    
-    function ReturIcon(){
-        if(props.icon=='CreditCardIcon'){
-            IconCard = <CreditCardIcon/>
-        }else if(props.icon=='CurrencyDollarIcon'){
-            IconCard = <CurrencyDollarIcon/>
-        }else if(props.icon=='OfficeBuildingIcon'){
-            IconCard = <OfficeBuildingIcon/>
-        }else{
-            IconCard = <CashIcon/>
-        }
-    }
-    ReturIcon()
+
     const valueFormatter = (number) => `$ ${new Intl.NumberFormat('ES', { style: 'currency', currency: 'COP' }).format(number)}`;
+    const deleteAccount =() =>{
+        console.log("Editar")
+    }
     return(
-        <div className={`flex ${props.color} rounded-lg`}>
+        <div className={`flex bg-${props.color}-500 relative rounded-lg`}>
+            <span 
+                onClick={deleteAccount}
+                className="grid place-items-center w-7 h-7 absolute right-2 top-2 hover:cursor-pointer">
+            <PencilIcon
+                className="w-4 hover:w-6"
+            />
+            </span>
+
             <input type="checkbox" id={props.name} value="" className="hidden peer" required=""/>
             <label htmlFor={props.name} className="flex items-center justify-between w-full text-gray-500 border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600  dark:peer-checked:text-gray-300 peer-checked:text-gray-600 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">                                   
                     <div className="w-20 flex justify-center items-center text-white">
-                        <div className="w-10">{IconCard}</div>
+                        <div className="w-10 text-white">
+                            {<GetIcons 
+                                icon={props.icon}
+                                size={"xl"} 
+                                color={"white"}
+                                variant={"simple"}
+                            />}
+                            </div>
                     </div>
                     <div className="w-full flex items-start flex-col">
                         <span className="text-gray-300 text-sm font-semibold mt-1">{props.name}</span>                     
