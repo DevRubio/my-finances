435b234cba72b139810e5340ec556f510526aaeb
oscar.franco <ofrubio0@gmail.com>
2023-10-24 07:22:51 -0500

    (ADD) Se crea componente GetIcons para utilizar los Icons

---
+++ src/components/GetIcons/index.jsx
@@ -0,0 +1,24 @@
+'use client'
+import { CashIcon, CreditCardIcon, CurrencyDollarIcon, OfficeBuildingIcon } from "@heroicons/react/outline";
+import { Icon } from "@tremor/react";
+
+export function GetIcons(props){
+
+    let IconCard     
+    function ReturIcon(){
+        if(props.icon=='CreditCardIcon'){
+            IconCard = CreditCardIcon
+        }else if(props.icon=='CurrencyDollarIcon'){
+            IconCard = CurrencyDollarIcon
+        }else if(props.icon=='OfficeBuildingIcon'){
+            IconCard = OfficeBuildingIcon
+        }else{
+            IconCard = CashIcon
+        }
+    }
+    ReturIcon()
+
+    return(
+        <Icon icon={IconCard} variant={props.variant} size={props.size} color={props.color}><CashIcon/></Icon>
+    )
+}
