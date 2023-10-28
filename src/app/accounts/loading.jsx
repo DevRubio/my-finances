a705e147de26172b3f220bdaabf105cc9ed23a8c
oscar.franco <ofrubio0@gmail.com>
2023-10-27 21:15:47 -0500

    (ADD) Se crea componente Loading

---
+++ src/app/accounts/loading.jsx
@@ -0,0 +1,7 @@
+import LoadingSkeleton from "@/components/LoadingSkeleton";
+
+export default function Loading() {
+    return <div className="w-full h-screen grid place-items-center">
+              <LoadingSkeleton/>
+           </div> 
+  }
