51b472bf68fef3c32f8cf3a0d49ac87dd0a5c39e
Mauricio Valencia <mvalenciau@sura.com.co>
2023-12-06 15:29:55 -0500

    (FIX) Se agregan condicionales en las utils para evitar datos undifined

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -1,10 +1,14 @@
 
-export function FormatMoney(value){
-    const valueFormater = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(value)
-    return valueFormater
+export function FormatMoney(value){    
+    if(value == undefined){
+        return null
+    }else{        
+        const valueFormater = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(value)
+        return valueFormater
+    }
 }
 
 export function FormatDate(date){
-    const newdateFormat = (date).toDate().toDateString() 
+    const newdateFormat = (date)?.toDate().toDateString() 
     return newdateFormat
 }
