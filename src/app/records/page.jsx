095cd2b9488937fb4e58effa08fd39bbfc2894ff
Mauricio Valencia <mvalenciau@sura.com.co>
2023-12-06 15:17:34 -0500

    (UPDATE) Se creacion div en page records y se le agregan estilos

--- src/app/records/page.jsx
+++ src/app/records/page.jsx
@@ -1,7 +1,18 @@
 import { Records } from "@/components/Records";
+import { Title, Button } from "@tremor/react";
 
 export default function Page(){
     return(
-        <Records/>
+        <div className="flex">
+            <div className="w-[20%] bg-white rounded-md p-3">
+                <Title className="m-3">Registros</Title>
+                <Button className="w-full">+ Agregar</Button>
+            </div>
+            <div className="w-[80%] ml-5">
+                <Records/>
+            </div>
+            
+        </div>
+        
     )
 }
