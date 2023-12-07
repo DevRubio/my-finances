f4f46d8c39123e2fb753964a7418f3c8a9568ca7
Mauricio Valencia <mvalenciau@sura.com.co>
2023-12-06 20:39:03 -0500

    (ADD) Se crea la funcion CalculateMonths

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -12,3 +12,13 @@ export function FormatDate(date){
     const newdateFormat = (date)?.toDate().toLocaleDateString('es-CO')
     return newdateFormat
 }
+
+export function CalculateMonths(fechaInicio, fechaFin) {
+    const inicio = new Date(fechaInicio * 1000)
+    const fin = new Date(fechaFin * 1000)
+
+    const añosDiferencia = fin.getFullYear() - inicio.getFullYear()
+    const mesesDiferencia = (fin.getMonth() + 1) - (inicio.getMonth() + 1)
+
+    return (añosDiferencia * 12) + mesesDiferencia;
+}
