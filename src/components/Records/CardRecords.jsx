19f67ee8ff7549943e84f606e1599e988de9286a
Mauricio Valencia <mvalenciau@sura.com.co>
2023-12-06 20:41:26 -0500

    (UUPDATE) Se utiliza Grid, se cambia los spam por div y se agregan spam para cambiar el tamaño del texto

--- src/components/Records/CardRecords.jsx
+++ src/components/Records/CardRecords.jsx
@@ -1,21 +1,22 @@
-import { BadgeDelta, Card, Text } from "@tremor/react";
-import { FormatDate, FormatMoney } from "@/app/lib/utils";
+import { BadgeDelta, Card, Text, Subtitle, Grid } from "@tremor/react";
+import { FormatDate, FormatMoney, CalculateMonths } from "@/app/lib/utils";
 
 export function CardRecords(props){ 
 
         
     return(
-        <Card decoration="top"> 
-            <div className="flex justify-between">
-                <span><Text>Nombre</Text>{props.name}</span>
-                <span><Text>Fecha de Inversion</Text>{FormatDate(props.date)}</span>
-                <span><Text>Inversion</Text>{FormatMoney(props.amount)}</span>
-                <span><Text>Ganancias</Text>
+        <Card decoration="top" className="mb-3">             
+            <Grid numItems={2} numItemsSm={4} numItemsLg={7} className="gap-2">
+                <div><Text>Nombre</Text><span className="text-sm">{props.name}</span></div>
+                <div><Text>Fecha</Text><span className="text-sm">{FormatDate(props.date)}</span></div>
+                <div className=""><Text>Inversion</Text><span className="text-sm">{FormatMoney(props.amount)}</span></div>
+                <div><Text>Adicion</Text><span className="text-sm">{FormatMoney(props.addition)}</span></div>
+                <div><Text>Ganancias</Text>
                 <BadgeDelta deltaType="moderateIncrease">{FormatMoney(props.investmentEarnings)}</BadgeDelta>             
-                </span>
-                <span><Text>Fecha de Ganancias</Text>{FormatDate(props.earningsDate)}</span>
-                <span><Text>Tasa</Text>{props.taxe}%</span>
-            </div>  
+                </div>
+                <div><Text>Tiempo</Text><span className="text-sm">{CalculateMonths(props.date, props.earningsDate)} Meses</span></div>
+                <div><Text>Tasa</Text><span className="text-sm">{props.taxe}%</span></div>
+            </Grid>  
         </Card>
     )
 }
