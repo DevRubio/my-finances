9e919f54101f8a0230a4658fd1828416ab829a5f
Oscar Rubio <ofrubio0@gmail.com>
2023-12-18 15:13:55 -0500

    (UPDATE) Se crea componente DetailsAccount para separar el detalle de la cuenta del serverComponet

--- src/app/accounts/detail/[id]/page.jsx
+++ src/app/accounts/detail/[id]/page.jsx
@@ -1,60 +1,18 @@
-'use client'
-import { Divider, Flex, Icon, Subtitle, Tab, TabGroup, TabList, TabPanel, TabPanels, Title } from "@tremor/react"
-import { Button, Card } from "flowbite-react"
-import { ArrowLeftIcon, CashIcon, PencilIcon } from "@heroicons/react/outline"
-import Link from "next/link"
-import { DeleteModal } from "@/components/Accounts/DeleteModal"
+import { getDetailsAccount } from "@/app/lib/actions"
+import { DetailsAccount } from "@/components/Accounts/DetailsAccount"
 
-
-export default function Detail({params}){
+export default async function Detail({params}){
+    
     const {id} = params
+    const account = await getDetailsAccount(id)
+   
     return(
-        <Flex flexDirection="col">
-            <Card className="m-3 w-full">
-                <div className="flex justify-between">
-                    <Flex justifyContent="start" className="gap-3">
-                        <Link href={`/accounts`}>
-                            <Icon icon={ArrowLeftIcon} size="lg" />
-                        </Link>
-                        <Subtitle>Detalles de Cuenta</Subtitle> 
-                    </Flex>
-                    <Flex justifyContent="end" className="gap-3">                        
-                    <Button gradientDuoTone="greenToBlue" outline> <PencilIcon className="mr-2 h-5 w-5"/> Editar </Button>
-                        <DeleteModal id={id}/>
-                    </Flex>
-                </div> 
-                <Divider/>
-                <Flex justifyContent="start" className="gap-3">
-                    <div className="w-24 h-24 text-white bg-blue-500 rounded-lg">
-                        <CashIcon/>
-                    </div>
-                    <div>
-                        <Subtitle>Nombre</Subtitle>  
-                        <Title>Efectivo</Title>
-                        <Subtitle>Tipo</Subtitle> 
-                        <Title>Inversion</Title> 
-                    </div> 
-                </Flex>
-                <Divider/>
-            </Card> 
-
-            <Card className="w-full">
-            <TabGroup>
-                    <TabList>
-                        <Tab>Saldo</Tab>
-                        <Tab>Registros</Tab>
-                    </TabList>          
-                <TabPanels>
-                    <TabPanel>
-                        A
-                    </TabPanel>
-                    <TabPanel>
-                        B
-                    </TabPanel>
-                </TabPanels>
-            </TabGroup>
-            </Card>           
-        </Flex>
-        
+          <DetailsAccount
+            id={id}
+            name={account.name}
+            type={account.type}
+            icon={account.icon}
+            color={account.color}
+          />    
     )
 }
