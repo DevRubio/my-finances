430959df5c77bfccc8481f3f752d3ec0ed12d5b2
Oscar Rubio <ofrubio0@gmail.com>
2023-12-31 14:54:10 -0500

    (ADD) Se crea componente AddRecords

--- src/app/records/page.jsx
+++ src/app/records/page.jsx
@@ -1,12 +1,16 @@
 import { Records } from "@/components/Records";
-import { Title, Button } from "@tremor/react";
+import { Title} from "@tremor/react";
+import { AddRecords } from "@/components/Records/AddRecords";
 
 export default function Page(){
     return(
         <div className="flex">
             <div className="w-[20%] bg-white rounded-md p-3">
-                <Title className="m-3">Registros</Title>
-                <Button className="w-full">+ Agregar</Button>
+                <Title className="m-3 text-gray-900">Registros</Title>               
+                    <div className="flex items-center justify-center bg-blue-500 cursor-pointer rounded-lg h-[40px] w-full">
+                        <AddRecords/>
+                    </div>                   
+             
             </div>
             <div className="w-[80%] ml-4">
                 <Records/>
