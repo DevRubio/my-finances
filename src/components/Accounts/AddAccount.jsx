654137cc5afeb0d4c713e28b8e1485d11f9e03a2
Oscar Rubio <ofrubio0@gmail.com>
2024-01-28 22:16:14 -0500

    (UPDATE) Se agrega react-hook-form para el formulario de AddAccount

--- src/components/Accounts/AddAccount.jsx
+++ src/components/Accounts/AddAccount.jsx
@@ -4,38 +4,33 @@ import { useState } from "react";
 import { Button, Modal, Checkbox, Label, Tooltip} from "flowbite-react";
 import { Flex, Select, SelectItem, Text, Title, TextInput} from "@tremor/react";
 import { CreditCardIcon, CashIcon, CurrencyDollarIcon, OfficeBuildingIcon } from "@heroicons/react/outline";
+import { useForm, Controller } from "react-hook-form";
 import { saveData } from "@/app/lib/actions";
+import { spamErrorForm } from "@/app/lib/utils";
 
 
-export function AddAccount({Colors}){
-    
-  const ValueInitial = {
-    name: "",
-    amount: "",
-}
+export function AddAccount({Colors}){    
+  
+const { register, formState:{errors}, handleSubmit, control, reset } = useForm()
   const [openModal, setOpenModal] = useState("")
   const props = { openModal, setOpenModal}
-  const [data, setData] = useState(ValueInitial)
-  const [valueColor, setValueColor]= useState("")
-  const [typeAccount, setTypeAccount]=useState("")
 
- const colorSave = {
-  color : valueColor
- }
- const typeAccountSave={
-  type : typeAccount
- }
-
-  const getInputs = (e)=>{    
-    const {name, value} = e.target   
-    setData({...data, [name]:value, ...colorSave, ...typeAccountSave})
+  const saveAccount = (data)=>{
+    const newData={
+      name: data.name,
+      color: data.color,
+      type: data.type,     
+      amount: parseInt(data.amount),
+      exclude: Boolean(data.exclude)
+    }   
+    saveData('accounts',newData)
+    onCloseModal()
   }
-  const saveAccount = (e)=>{
-    e.preventDefault()
-    saveData(data)
+
+  const onCloseModal =()=>{
     props.setOpenModal(undefined)
+    reset()
   }
-  
 
   
   return (
@@ -51,7 +46,7 @@ export function AddAccount({Colors}){
         show={props.openModal === "form-elements"}
         size="md"
         popup
-        onClose={() => props.setOpenModal(undefined)}
+        onClose={() => onCloseModal()}
       >
         <Modal.Header>
             <div className="m-2">
@@ -60,21 +55,35 @@ export function AddAccount({Colors}){
 
         </Modal.Header>
         <Modal.Body>
-          <form onSubmit={saveAccount}>
+          <form onSubmit={handleSubmit(saveAccount)}>
           <div className="space-y-6">    
           <div>
             <Flex className="gap-2">
             <div className="">               
                 <Text >Nombre</Text>
-                <TextInput placeholder="Nombre de la cuenta" type="text" name="name" value={data.name} onChange={getInputs} required />              
+                <TextInput placeholder="Nombre de la cuenta" type="text" name="name" {...register('name',{
+                  required: true
+                })}/>    
+              {errors.name?.type === 'required' && spamErrorForm('El nombre es requerido')}            
             </div>
             <div className="">               
                 <Text >Color</Text>
-                <Select name="Color" value={valueColor} onValueChange={setValueColor}>
-                  {Colors?.map((color)=>(                    
-                    <SelectItem key={color.id} name={color.name} value={color.name}>{color.name}</SelectItem>
-                  ))}    
-                </Select>
+                <Controller 
+                  name="color"
+                  control={control}
+                  rules={{required: true}}
+                  render={({field})=>(
+                    <Select
+                      value={field.value}
+                      onChange={(value)=>field.onChange(value)}
+                    >
+                      {Colors?.map((color)=>(
+                        <SelectItem key={color.id} value={color.name}>{color.name}</SelectItem>
+                      ))}
+                    </Select>
+                  )}
+                />
+                {errors.color?.type === "required" && spamErrorForm('El color es requerido')}
             </div>
             </Flex>
             </div>      
@@ -82,29 +91,40 @@ export function AddAccount({Colors}){
               <div className="mb-2 block">
                 <Text>Tipo de Cuenta</Text>
               </div>
-              <Select icon={CashIcon} value={typeAccount} onValueChange={setTypeAccount}>              
-                <SelectItem value="cash" icon={CashIcon}>              
-                 Efectivo
-                </SelectItem>
-                <SelectItem value="creditCard" icon={CreditCardIcon}>
-                Tarjeta de Credito
-                </SelectItem>
-                <SelectItem value="investment" icon={CurrencyDollarIcon}>
-                Inversion
-                </SelectItem>
-                <SelectItem value="BankAccount" icon={OfficeBuildingIcon}>
-                Cuenta Bancaria
-                </SelectItem>
-            </Select>
+              <Controller 
+                name="type"
+                control={control}
+                rules={{required: true}}
+                render={({field})=>(
+                  <Select icon={CashIcon} value={field.value} onChange={(value)=>field.onChange(value)}>              
+                  <SelectItem value="cash" icon={CashIcon}>              
+                   Efectivo
+                  </SelectItem>
+                  <SelectItem value="creditCard" icon={CreditCardIcon}>
+                  Tarjeta de Credito
+                  </SelectItem>
+                  <SelectItem value="investment" icon={CurrencyDollarIcon}>
+                  Inversion
+                  </SelectItem>
+                  <SelectItem value="BankAccount" icon={OfficeBuildingIcon}>
+                  Cuenta Bancaria
+                  </SelectItem>
+              </Select>
+                )}
+              />
+            {errors.type?.type === 'required' && spamErrorForm('El tipo de cuenta es requerido')}
             </div> 
             <div>
                 <div className="mb-2 block">
                     <Text>Monto Inicial</Text>
                 </div>
-                <TextInput type="number" name="amount" value={data.amount} onChange={getInputs} placeholder="0"/>
+                <TextInput placeholder="0" {...register('amount',{
+                  required: true
+                })}/>
+                {errors.amount?.type === 'required' && spamErrorForm('El monto es requerido')}
                 </div>    
                 <div className="flex items-center gap-2">
-                    <Checkbox id="exclude" name="exclude" value={data.exclude} onChange={getInputs} />
+                    <Checkbox {...register('exclude')} />
                     <Tooltip style="light" content="Esta opción excluye los registros de las graficas">
                     <Label htmlFor="exclude">
                     Excluir de las estadísticas
