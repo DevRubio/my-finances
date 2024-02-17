9f5744204cdd76baf24f981e432f25f1b8d7470c
Oscar Rubio <ofrubio0@gmail.com>
2024-02-16 21:35:08 -0500

    (UPDATE) Se cambia el if de la funcion FormatMoney por un ternario, Se cambia el parametro Earnings por finalBalance en la funcion CalTaxe, Se crea la funcion CalTaxeCripto para calcular las ganancias de las criptos, Se actualiza funcion calcDeltaType

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -1,12 +1,10 @@
 import { Italic } from "@tremor/react"
 
-export function FormatMoney(value){    
-    if(value == undefined){
-        return null
-    }else{        
-        const valueFormater = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(value)
-        return valueFormater
-    }
+export function FormatMoney(value){ 
+
+    const valueFormater = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(value ? value : 0)
+    return valueFormater
+    
 }
 
 export function FormatDate(date){
@@ -14,8 +12,7 @@ export function FormatDate(date){
     return newdateFormat
 }
 
-export function CalculateMonths(fechaInicio, fechaFin) {
-
+export function CalculateMonths(fechaInicio, fechaFin) {   
     if(fechaInicio && fechaFin){
         const inicio = new Date(fechaInicio * 1000)
         const fin = new Date(fechaFin * 1000)    
@@ -33,30 +30,58 @@ export const spamErrorForm =(err)=>{
     return <Italic className="text-red-600" >{err}</Italic>
 }
 
-export const CalTaxe = (Earnings, total) =>{
-    if(Earnings != 0){
-        return ((Earnings/total)*100/1).toFixed(2)
+export const CalTaxe = (addition, finalBalance) =>{
+    if(finalBalance){
+        return (((finalBalance-addition)/addition)*100/1).toFixed(2)
     }
     return 0
 }
 
+export const CalTaxeCripto = (priceCompra, PriceActual) =>{
+    const diferenciaPrecio = PriceActual - priceCompra;
+    const porcentajeGanancia = (diferenciaPrecio / priceCompra) * 100
+    return porcentajeGanancia.toFixed(2)
+}
+
+export const CalEarningCripto = (compra, priceCompra, priceBTC)=>{
+    const Earnings = (compra * CalTaxeCripto(priceCompra, priceBTC))/100
+    return FormatMoney(Earnings)
+}
+
+export const CalEarning = (finalBalance, addition) =>{
+    if(finalBalance != 0 ){
+       return FormatMoney(finalBalance - addition)
+    }
+    return FormatMoney(0)
+
+    
+}
+
 export const StringToDate=(date)=>{
-    const partsDate = date.split('/')
-    const day = parseInt(partsDate[0], 10)
-    const mount = parseInt(partsDate[1], 10)-1
-    const year = parseInt(partsDate[2],10)
+    if(date){
+        const partsDate = date.split('/')
+        const day = parseInt(partsDate[0], 10)
+        const mount = parseInt(partsDate[1], 10)-1
+        const year = parseInt(partsDate[2],10)
+    
+        const newDate = new Date(year, mount, day)
+    
+        return newDate
+    }
 
-    const newDate = new Date(year, mount, day)
-
-    return newDate
 }
 
 export const calcDeltaType=(taxe)=>{
-    if(taxe > 1){
-        return "moderateIncrease"
-      }else if(taxe < -1){
-        return "moderateDecrease"
-      }else{
-        return "unchanged"
-      }
+    
+    if (taxe >= 10) {
+        return "increase";
+    } else if (taxe >= 5) {
+        return "moderateIncrease";
+    } else if (taxe >= 0) {
+        return "unchanged";
+    } else if (taxe >= -4) {
+        return "moderateDecrease";
+    } else {
+        return "decrease"; 
+    }
   }
