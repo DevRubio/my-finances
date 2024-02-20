e812889c9cdb978a2d47994ddab116b2d31ce0bf
Oscar Rubio <ofrubio0@gmail.com>
2024-02-20 10:05:11 -0500

    (UPDATE) Se llama la funcion getConfig para obtener los tipos de inversion

--- src/app/records/page.jsx
+++ src/app/records/page.jsx
@@ -1,11 +1,12 @@
 import { Records } from "@/components/Records";
 import { Title} from "@tremor/react";
 import { AddRecords } from "@/components/Records/AddRecords";
-import { getAccounts } from "../lib/actions";
+import { getAccounts, getConfig } from "../lib/actions";
 
 
 export default async function Page(){
     const Accounts = await getAccounts()
+    const typeInvestment = await getConfig('typeInvestment')
     return(
         <div className="flex">
             <div className="w-[20%] bg-white rounded-md p-3">
@@ -13,8 +14,9 @@ export default async function Page(){
                     <div className="flex items-center justify-center bg-blue-500 cursor-pointer rounded-lg h-[40px] w-full">
                         <AddRecords
                             Accounts={Accounts}
+                            typeInvestment={typeInvestment}
                         />
-                    </div>                   
+                    </div>               
              
             </div>
             <div className="w-[80%] ml-4">
