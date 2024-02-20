1063feeb75a334d10aca6bfdc22e4cd7420df010
Oscar Rubio <ofrubio0@gmail.com>
2024-02-20 14:12:10 -0500

    (UPDATE) Se importa el Link y el PencilIcon para editar los registros CRIPTO

--- src/components/Records/TableRecordsCripto.jsx
+++ src/components/Records/TableRecordsCripto.jsx
@@ -9,6 +9,8 @@ import {
     TableRow,
   } from '@tremor/react';
   import { ModalDelete } from "./ModalDelete";
+  import Link from "next/link";
+  import { PencilIcon } from "@heroicons/react/outline";
   
 
 
@@ -21,19 +23,19 @@ export function TableRecordsCripto({recordsCripto, price}){
     }
     return(
         <div>
-                 <Table className="mt-8">
-        <TableHead>
-          <TableRow className="border-b border-tremor-border dark:border-dark-tremor-border">
-            {CellDataTable('Criptomoneda')}
-            {CellDataTable('Fecha de Compra')}
-            {CellDataTable('Total Compra')}
-            {CellDataTable('Total Cripto')}
-            {CellDataTable('Valor Cripto')}
-            {CellDataTable('Ganancias')}
-            {CellDataTable('Tasa')}    
-            {CellDataTable('Accion')}   
-          </TableRow>
-        </TableHead>
+        <Table className="mt-8">
+          <TableHead>
+            <TableRow className="border-b border-tremor-border dark:border-dark-tremor-border">
+              {CellDataTable('Criptomoneda')}
+              {CellDataTable('Fecha de Compra')}
+              {CellDataTable('Total Compra')}
+              {CellDataTable('Total Cripto')}
+              {CellDataTable('Valor Cripto')}
+              {CellDataTable('Ganancias')}
+              {CellDataTable('Tasa')}    
+              {CellDataTable('Accion')}   
+            </TableRow>
+          </TableHead>
         <TableBody>
           {recordsCripto.map((item) => (
             <TableRow key={item.workspace}>
@@ -46,11 +48,16 @@ export function TableRecordsCripto({recordsCripto, price}){
               <TableCell>{FormatMoney(item.price)}</TableCell>
               <TableCell>{ CalEarningCripto(item.addition, item.price, priceBTC)}</TableCell>
               <TableCell><BadgeDelta deltaType={calcDeltaType(CalTaxeCripto(item.price, priceBTC))}>{CalTaxeCripto(item.price, priceBTC)} %</BadgeDelta></TableCell>
-              <TableCell>
+              <TableCell className="flex">
                 <ModalDelete
                   id={item.id}
                   collection="CRIPTO"
                 />
+                <Link href={`/records/edit/CRIPTO/${item.id}`}>
+                  <PencilIcon
+                    className="w-6 hover:w-8 hover:text-yellow-500 hover:cursor-pointer"
+                  />                            
+                </Link>
               </TableCell>
             </TableRow>
           ))}
